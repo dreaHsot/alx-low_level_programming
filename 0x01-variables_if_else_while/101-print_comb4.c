cfa3cff6354bd7b61600248c703d45355705985c
damilare <damilaresotunde@yahoo.com>
2023-06-17 07:53:17 -0700

    3 digit combination without repeatition

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,8 +11,11 @@ int main(void)
 	int a, b, c;
 
 	for (a = 0; a < 10; a++)
+	{
 		for (b = a + 1; b < 10; b++)
+		{
 			for (c = b + 1; c < 10; c++)
+			{
 				if (a != b != c)
 				{
 					putchar(a + '0');
@@ -24,6 +27,9 @@ int main(void)
 						putchar(' ');
 					}
 				}
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
