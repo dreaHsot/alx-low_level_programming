9ac93f26650736c56d21d295750520ef08e30c46
damilare <damilaresotunde@yahoo.com>
2023-06-20 15:47:09 -0700

    alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+  *
+  */
+
+void print_alphabet_x10(void)
+{
+	char alphabets;
+	int n;
+
+	for (n = 0; n < 10; n++)
+		for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		{
+			_putchar(alphabets);
+		}
+	_putchar('\n');
+}
