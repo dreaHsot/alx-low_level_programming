170879adebc6f1f1cf50cadd1612e9217ac46c4b
damilare <damilaresotunde@yahoo.com>
2023-06-26 20:17:49 -0700

    from halfway

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * puts_half - prints the second half of the string
+  * @str: input string to be halved
+  *
+  */
+
+void puts_half(char *str)
+{
+	int l = 0;
+	int i;
+
+	while (str[l] != '\0')
+		l++;
+	for (i = l / 2; i < l; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
