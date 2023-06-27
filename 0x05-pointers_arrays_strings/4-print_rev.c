65c277ebf121da7a308b4dfbab24129badf34529
damilare <damilaresotunde@yahoo.com>
2023-06-26 17:25:24 -0700

    reverse printing

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_rev - prints a string followed by a new line
+  * @s: input string to be reversed
+  *
+  */
+
+void print_rev(char *s)
+{
+	int i, l;
+
+	while (s[l] != '\0')
+		(l++);
+
+	for (i = l - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
