c36683c7367629c00a867fdac1c9a575d60999e0
damilare <damilaresotunde@yahoo.com>
2023-07-03 10:37:26 -0700

    memory setting

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @n: number of bytes to be filled
+ * @s: pointer to the memory area ro be filled
+ * @b: value to fill the memory area
+ * Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+@alx decided to grill me more on pointers, arrays and strings
