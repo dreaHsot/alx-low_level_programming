260e72a9c87e9822fbbead85bfa788c6cf83d4fd
dreaHsot <damilaresotunde@yahoo.com>
2023-08-07 17:46:28 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of file to be read
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf = NULL;
+	ssize_t tread, twritten;
+	FILE *fp = fopen(filename, "r");
+
+	if (fp == NULL || filename == NULL)
+		return (0);
+
+	buf = (char *)malloc(letters + 1);
+	if (buf == NULL)
+	{
+		fclose(fp);
+		return (0);
+	}
+
+	tread = fread(buf, sizeof(char), letters, fp);
+	if (tread == 0 || ferror(fp))
+	{
+		free(fp);
+		fclose(fp);
+		return (0);
+	}
+
+	buf[tread] = '\0';
+
+	twritten = fwrite(buf, sizeof(char), tread, stdout);
+	if (twritten != tread)
+	{
+		free(buf);
+		fclose(fp);
+		return (0);
+	}
+
+	free(buf);
+	fclose(fp);
+	return (twritten);
+}
