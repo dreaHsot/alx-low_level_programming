94e879cdfc6892838dd271a0f861cb17c9c38388
damilare <damilaresotunde@yahoo.com>
2024-02-08 08:05:55 -0800

    code tidied up

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,10 +16,12 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 
 	while (b[i] != '\0')
+	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		uint = uint * 2 + (b[i] - '0');
 		i++;
+	}
 
 	return (uint);
 }
